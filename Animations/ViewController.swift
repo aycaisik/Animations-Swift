f623a8097726218d116848afe98b7f180bea8983
Ayça Işık <117450558+aycaisik@users.noreply.github.com>
2023-01-25 20:08:21 +0300

    Added animations.

--- Animations/ViewController.swift
+++ Animations/ViewController.swift
@@ -8,10 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var animatableView: UIView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        UIView.animate(withDuration: 1) {
+            self.view.alpha = 0.5
+        }
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        UIView.animate(withDuration: 2) {
+            self.animatableView.backgroundColor = .yellow
+        }
     }
 
 
